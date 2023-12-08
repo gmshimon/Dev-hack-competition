ab626385f2e6eff5fe2918e293ca8e300f1a2f9e
nabilahsnn <simon.rosedale99@gmail.com>
2023-12-09 04:13:32 +0800

    change color

--- Mobile-css-frontend/mobile-app/Components/CustomerExpenses/ExpenseChart.jsx
+++ Mobile-css-frontend/mobile-app/Components/CustomerExpenses/ExpenseChart.jsx
@@ -11,26 +11,37 @@ import { View, Text, Dimensions } from "react-native";
 
 const chartConfig = {
   backgroundGradientFromOpacity: 0,
-  backgroundGradientToOpacity: 0.5,
-  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-  strokeWidth: 2, // optional, default 3
-  barPercentage: 0.5,
+  backgroundGradientToOpacity: 0.0,
+  color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`,
+  strokeWidth: 3, // optional, default 3
+  barPercentage: 0.8,
+  fillShadowGradient: 'rgba(26, 255, 1, 0.5)',
+  fillShadowGradientOpacity: 1,
+  propsForBackgroundLines: {
+    strokeDasharray: "", // background line dashed setting
+  },
+  propsForLabels: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color:"black"
+  },
   useShadowColorFromDataset: false, // optional
+  colorScale: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
 };
 
 export default function ExpenseChart() {
   const data = {
-    labels: ["Swim", "Bike", "Run"], // optional
+    labels: ["Water", "Gas", "sewerage"], // optional
     data: [0.4, 0.6, 0.8],
   };
   return (
     <View>
-      <Text>Bezier Line Chart</Text>
       <ProgressChart
         data={data}
-        width={Dimensions.get("window").width}
+        style={{paddingRight:12}}
+        width={Dimensions.get("window").width-15}
         height={220}
-        strokeWidth={16}
+        strokeWidth={15}
         radius={32}
         chartConfig={chartConfig}
         hideLegend={false}
